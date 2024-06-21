937c7f15ab752277085c375d063cc9802b78b2c8
Luis Felipe dos Santos Andrade <89539253+luisfelipedsa@users.noreply.github.com>
2024-06-21 17:14:33 -0300

    sintaxesegunda error

--- app/(dashboard)/orders/page.tsx
+++ app/(dashboard)/orders/page.tsx
@@ -1,40 +1,42 @@
-"use client"
+"use client";
 
-import { DataTable } from "@/components/custom ui/DataTable"
-import Loader from "@/components/custom ui/Loader"
-import { columns } from "@/components/orders/OrderColumns"
-import { Separator } from "@/components/ui/separator"
+import { DataTable } from "@/components/custom ui/DataTable";
+import Loader from "@/components/custom ui/Loader";
+import { columns } from "@/components/orders/OrderColumns";
+import { Separator } from "@/components/ui/separator";
 
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 
 const Orders = () => {
-  const [loading, setLoading] = useState(true)
-  const [orders, setOrders] = useState([])
+  const [loading, setLoading] = useState(true);
+  const [orders, setOrders] = useState([]);
 
   const getOrders = async () => {
     try {
-      const res = await fetch(`${process.env.ADMIN_DASHBOARD_URL}/api/orders`)
-      const data = await res.json()
-      setOrders(data)
-      setLoading(false)
+      const res = await fetch(`${process.env.ADMIN_DASHBOARD_URL}/api/orders`);
+      const data = await res.json();
+      setOrders(data);
+      setLoading(false);
     } catch (err) {
-      console.log("[orders_GET]", err)
+      console.log("[orders_GET]", err);
     }
-  }
+  };
 
   useEffect(() => {
-    getOrders()
-  }, [])
+    getOrders();
+  }, []);
 
-  return loading ? <Loader /> : (
+  return loading ? (
+    <Loader />
+  ) : (
     <div className="px-10 py-5">
       <p className="text-heading2-bold">Pedidos</p>
-      <Separator className="bg-grey-1 my-5"/>
+      <Separator className="bg-grey-1 my-5" />
       <DataTable columns={columns} data={orders} searchKey="_id" />
     </div>
-  )
-}
+  );
+};
 
 export const dynamic = "force-dynamic";
 
-export default Orders
+export default Orders;
